6a7e9845f054105f8855a25078cd477a06149fa3
rankele <rankele@tamu.edu>
2022-02-03 21:28:14 -0600

    fix date test

--- book_app/spec/feature/integration_spec.rb
+++ book_app/spec/feature/integration_spec.rb
@@ -45,12 +45,14 @@ RSpec.describe 'Create book with title/date', type: :feature do
   scenario 'valid inputs and shown' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    select '2020-1-17', :from => 'published_date'
+    select '2020', :from => 'book_published_date_1i'
+    select 'January', :from => 'book_published_date_2i'
+    select '17', :from => 'book_published_date_3i'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
     click_on 'Show'
     expect(page).to have_content('harry potter')
-    expect(page).to have_content('2020-1-17')
+    expect(page).to have_content('2020-01-17')
   end
 end
